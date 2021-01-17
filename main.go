7dd356377b2baaa8e227991a7794228f4e37e8c7
neulhan <sting7675@gmail.com>
2021-01-17 22:19:43 +0900

    change package: -iris +gin-gonic

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"github.com/kataras/iris/v12"
+	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
-	app := iris.New()
-	err := app.Listen(":8000")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run()
+
 	if err != nil {
 		log.Fatal(err)
 	}
