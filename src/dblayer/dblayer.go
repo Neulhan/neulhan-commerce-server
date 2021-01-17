85dba6ce6d8e3ae36df9d43d2ea34cf82c802389
neulhan <sting7675@gmail.com>
2021-01-17 22:22:45 +0900

    create dblayer: create dblayer interface

--- src/dblayer/dblayer.go
+++ src/dblayer/dblayer.go
@@ -1,1 +1,15 @@
 package dblayer
+
+import "neulhan-commerce-server/src/models"
+
+type DBLayer interface {
+	GetAllProduct() ([]models.Product, error)
+	GetPromos() ([]models.Product, error)
+	GetCustomerByName(string) (models.Customer, error)
+	GetCustomerByID(int) (models.Customer, error)
+	GetProduct(uint) (models.Product, error)
+	AddUser() (models.Customer, error)
+	SignInUser(username, password string) (models.Customer, error)
+	SignOutUserByID(int) error
+	GetCustomerOrdersByID(int) ([]models.Order, error)
+}
