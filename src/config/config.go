776bc0138d5ce5bf40f6e18d8b9a745e1e91464b
neulhan <sting7675@gmail.com>
2021-01-18 23:51:45 +0900

    add func: create viper get env function

---
+++ src/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+func GetEnv(key string) string {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	value, ok := viper.Get(key).(string)
+
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+
+	return value
+}
