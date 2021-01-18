56bd9139c9425d5a42e8f0cb15088541b02faca3
neulhan <sting7675@gmail.com>
2021-01-18 23:54:39 +0900

    set env: get port from env

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"log"
+	"neulhan-commerce-server/src/config"
 	"neulhan-commerce-server/src/rest"
 )
 
 func main() {
 	log.Println("START SERVER")
-	log.Fatal(rest.RunAPI(":8000"))
+	log.Fatal(rest.RunAPI(config.GetEnv("PORT")))
 }
