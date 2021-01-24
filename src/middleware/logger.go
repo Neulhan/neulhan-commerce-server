800dc4b1cd363a89602280d2102528d0c45061f0
neulhan <sting7675@gmail.com>
2021-01-24 23:30:51 +0900

    update logger: remove useless line

--- src/middleware/logger.go
+++ src/middleware/logger.go
@@ -1,17 +1,16 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func CustomMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 요청 전 실행
-		fmt.Println("----------------")
+		//fmt.Println("----------------")
 		// 요청 실행
 		c.Next()
 		// 요청 후 실행
-		fmt.Println("----------------")
+		//fmt.Println("----------------")
 	}
 }
