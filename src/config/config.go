4d57de0121ef32d131dd9652460e2cb3e08ae56d
neulhan <sting7675@gmail.com>
2021-01-24 23:28:38 +0900

    change config: change os to viper

--- src/config/config.go
+++ src/config/config.go
@@ -1,9 +1,29 @@
 package config
 
 import (
+	"github.com/spf13/viper"
+	"log"
 	"os"
 )
 
 func GetEnv(key string) string {
-	return os.Getenv(key)
+	if os.Getenv("GIN_MODE") == "release" {
+		viper.SetConfigFile("prd.env")
+	} else {
+		viper.SetConfigFile("dev.env")
+	}
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Printf("Error while reading config file %s", err)
+	}
+
+	value, ok := viper.Get(key).(string)
+
+	if !ok {
+		log.Fatalf("Invalid type assertion %s", value)
+	}
+
+	return value
 }
