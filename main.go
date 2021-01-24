f1106cb3c8555b137ba48e08dbe7f9baaf0d4411
neulhan <sting7675@gmail.com>
2021-01-24 23:29:34 +0900

    update main: show env at start

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"log"
 	"neulhan-commerce-server/src/config"
 	"neulhan-commerce-server/src/rest"
+	"os"
 )
 
 func main() {
-	log.Println("START SERVER")
+	log.Printf("[%s] START SERVER ON %s", os.Getenv("GIN_MODE"), config.GetEnv("PORT"))
 	log.Fatal(rest.RunAPI(config.GetEnv("PORT")))
 }
