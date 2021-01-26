bae9b9b818947fad2570f27a8721d42ed6889d45
neulhan <sting7675@gmail.com>
2021-01-27 01:27:34 +0900

    update orm: add update, create orm for product

--- src/dblayer/productORM.go
+++ src/dblayer/productORM.go
@@ -1,17 +1,30 @@
 package dblayer
 
 import (
+	"gorm.io/gorm"
 	"neulhan-commerce-server/src/models"
 )
 
-func (db *dblayer.DBORM) GetAllProducts() (products []models.Product, err error) {
+func (db *DBORM) GetProducts() (products []models.Product, err error) {
 	return products, db.Find(&products).Error
 }
 
-func (db *dblayer.DBORM) GetPromos() (products []models.Product, err error) {
+func (db *DBORM) CreateProduct(product models.Product) (models.Product, error) {
+	return product, db.Create(&product).Error
+}
+
+func (db *DBORM) UpdateProduct(updateProduct models.Product) (product models.Product, err error) {
+	return updateProduct, db.Table("Product").Where(models.Product{
+		Model: gorm.Model{
+			ID: updateProduct.ID,
+		},
+	}).Save(&updateProduct).Error
+}
+
+func (db *DBORM) GetPromos() (products []models.Product, err error) {
 	return products, db.Where("promotion IS NOT NULL").Find(&products).Error
 }
 
-func (db *dblayer.DBORM) GetProductByID(id uint) (product models.Product, err error) {
+func (db *DBORM) GetProductByID(id uint) (product models.Product, err error) {
 	return product, db.First(product, id).Error
 }
