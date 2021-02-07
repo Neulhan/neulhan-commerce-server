9fdb0cd4368c8a11391de478825e068c3a59eb22
neulhan <sting7675@gmail.com>
2021-02-07 12:40:02 +0900

    rename package: auth -> jwt

---
+++ src/jwt/jwt.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"neulhan-commerce-server/src/config"
+	"time"
+)
+
+type Claims struct {
+	UserID int
+	jwt.StandardClaims
+}
+
+var expirationTime = time.Minute * 60
+
+func CreateToken(userID int) (string, error) {
+	expirationTime := time.Now().Add(expirationTime)
+
+	claims := &Claims{
+		UserID: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(config.GetEnv("SECRET_KEY")))
+
+	if err != nil {
+		return "", fmt.Errorf("token signed error")
+	}
+	return tokenString, nil
+}
+
+func ParseToken(myToken string) (*Claims, error) {
+	claims := &Claims{}
+
+	_, err := jwt.ParseWithClaims(myToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.GetEnv("SECRET_KEY")), nil
+	})
+
+	if err != nil {
+		return claims, err
+	}
+
+	return claims, nil
+}
